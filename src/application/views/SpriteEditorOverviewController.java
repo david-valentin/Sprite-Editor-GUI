bc5365a4b947a81cd869a80c03503bf26aedf222
David Valentin <davidvalentin@Davids-MacBook-Air.local>
2017-11-21 21:03:00 +0000

    created simple gui for the application

---
+++ src/application/views/SpriteEditorOverviewController.java
@@ -0,0 +1,38 @@
+package application.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * 	Overall Architecture of the App:
+ * 
+ * 	MODELS:
+ * 	1. Sprite Interface or Class = can be a png file or editable 
+ * 	2. Brush Interface
+ * 	3. 
+ * 
+ * 	VIEWS:
+ * 	1. SpriteEditorOverview
+ * 	2. Rootlayout 
+ * 
+ * 	CONTROLLERS:
+ * 	1. SpriteEditOverviewController
+ * 	> Controls the general behavior of the application
+ * 	2. CanvasController
+ * 	> Controls the canvas and updates it with a png or a specific color
+ * 
+ * FactoryDesignPattern?
+ * */
+
+// TODO:
+//	1. Set up a grid system based off what
+//	2. 
+
+
+public class SpriteEditorOverviewController {
+
+}
